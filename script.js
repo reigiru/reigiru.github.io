203097657a0acae2ae25eb6c54540da204bd28e4
senselessgod <saspupovich454@gmail.com>
2021-06-11 02:49:53 +0300

    1.7
    new characters

--- script.js
+++ script.js
@@ -28,29 +28,14 @@ AOS.init({
   
   });
 
-  // Get DOM Elements
-const modal = document.querySelector('#my-modal');
-const modalBtn = document.querySelector('#modal-btn');
-const closeBtn = document.querySelector('.close');
-
-// Events
-modalBtn.addEventListener('click', openModal);
-closeBtn.addEventListener('click', closeModal);
-window.addEventListener('click', outsideClick);
-
-// Open
-function openModal() {
-  modal.style.display = 'block';
-}
-
-// Close
-function closeModal() {
-  modal.style.display = 'none';
-}
-
-// Close If Outside Click
-function outsideClick(e) {
-  if (e.target == modal) {
-  modal.style.display = 'none';
-  }
-}
+  
+  $(".open_popup").click(function () {
+    $(this).parent(".popup_main").children(".popup_body").addClass("popup_body_show");
+    });
+  $(".popup_close").click(function () {
+    $(".popup_body").removeClass("popup_body_show");
+    });
+  $(".popup_back").click(function () {
+    $(".popup_body").removeClass("popup_body_show");
+    });
+ 
